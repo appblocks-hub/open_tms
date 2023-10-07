3d544bf82f6a457cfa6aec53bc28141b4e6e0dd4
devthalal <thalal.hassan@neoito.com>
2023-10-07 12:29:09 +0530

    feat: open_tms_close_ticket

---
+++ open_tms_backend/open_tms_close_ticket/README.md
@@ -0,0 +1,1 @@
+# open_tms_close_ticket
---
+++ open_tms_backend/open_tms_close_ticket/index.js
@@ -0,0 +1,21 @@
+import { shared } from '@appblocks/node-sdk'
+
+const handler = async (event) => {
+  const { req, res } = event
+  const { sendResponse } = await shared.getShared()
+
+  // health check
+  if (req.params.health === 'health') {
+    return sendResponse(res, 200, { message: 'Health check success' })
+  }
+
+  console.log(req.user);
+  console.log(req.body);
+  /**
+   * Add close ticket logic here
+   */
+
+  return sendResponse(res, 200, { message: 'Successfully closed ticket' })
+}
+
+export default handler
