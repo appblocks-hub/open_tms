7d54f6fd3273c9cb2569be34e079e9c358cd4069
Frederic Thomson <frederic@neoito.com>
2023-10-13 17:24:02 +0530

    Fix: some DB issues inn BE

--- open_tms_auth/open_tms_auth_be_reset_password/index.js
+++ open_tms_auth/open_tms_auth_be_reset_password/index.js
@@ -1,6 +1,6 @@
-import { shared } from "@appblocks/node-sdk";
-import { compare } from "bcrypt";
-import { hash, genSalt } from "bcrypt";
+import { shared } from '@appblocks/node-sdk'
+import { compare } from 'bcrypt'
+import { hash, genSalt } from 'bcrypt'
 
 /**
  * @swagger
@@ -28,9 +28,9 @@ import { hash, genSalt } from "bcrypt";
  *         description: Created
  *       '200':
  *         description: Ok
-*/
+ */
 const handler = async (event) => {
-  const { req, res } = event;
+  const { req, res } = event
 
   const {
     sendResponse,
@@ -39,35 +39,35 @@ const handler = async (event) => {
     redis,
     validateRequestMethod,
     checkHealth,
-  } = await shared.getShared();
+  } = await shared.getShared()
 
   try {
     // health check
-    if (checkHealth(req, res)) return;
-    await validateRequestMethod(req, ["PUT"]);
+    if (checkHealth(req, res)) return
+    await validateRequestMethod(req, ['PUT'])
 
-    const requestBody = req.body;
+    const requestBody = req.body
 
     if (
       isEmpty(requestBody) ||
-      !requestBody.hasOwnProperty("user_auth_token") ||
-      !requestBody.hasOwnProperty("new_password")
+      !requestBody.hasOwnProperty('user_auth_token') ||
+      !requestBody.hasOwnProperty('new_password')
     ) {
       return sendResponse(res, 400, {
-        message: "Please enter AuthToken and new password.",
-      });
+        message: 'Please enter AuthToken and new password.',
+      })
     }
 
-    const { user_auth_token, new_password } = requestBody;
+    const { user_auth_token, new_password } = requestBody
 
-    if (!redis.isOpen) await redis.connect();
-    const user_account_id = await redis.get(user_auth_token);
-    await redis.disconnect();
+    if (!redis.isOpen) await redis.connect()
+    const user_account_id = await redis.get(user_auth_token)
+    await redis.disconnect()
 
     if (!user_account_id) {
       return sendResponse(res, 400, {
-        message: "Provided auth token does not exist.",
-      });
+        message: 'Provided auth token does not exist.',
+      })
     }
 
     function isPasswordValid(password) {
@@ -77,70 +77,71 @@ const handler = async (event) => {
       // It should contain at least one digit
       // It should contain at least one special character
       const passwordPattern =
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\d!@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,}$/;
-      return passwordPattern.test(password);
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\d!@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,}$/
+      return passwordPattern.test(password)
     }
 
     if (!isPasswordValid(new_password)) {
       return sendResponse(res, 400, {
-        message: "Please provide a valid password.",
-      });
+        message: 'Please provide a valid password.',
+      })
     }
 
     const user_account = await prisma.user_account.findFirst({
       where: {
         id: user_account_id,
       },
-    });
+    })
 
     if (!user_account) {
       return sendResponse(res, 400, {
-        message: "User account does not exist for the provided auth token.",
-      });
+        message: 'User account does not exist for the provided auth token.',
+      })
     }
 
-    const { user } = user_account;
+    const { user } = user_account
 
     if (!isPasswordValid(new_password)) {
       return sendResponse(res, 400, {
-        message: "Please provide a valid password",
-      });
+        message: 'Please provide a valid password',
+      })
     }
 
     if (await compare(new_password, user_account.password_hash)) {
       return sendResponse(res, 400, {
-        message: "New password cannot be the same as the previous password.",
-      });
+        message: 'New password cannot be the same as the previous password.',
+      })
     }
 
-    const salt = await genSalt(10);
-    const newPassword = await hash(new_password, salt);
+    const password_salt = await genSalt(10)
+    const password_hash = await hash(new_password, password_salt)
     const updatedUserAccount = await prisma.user_account.update({
       where: {
         id: user_account.id,
       },
       data: {
-        password: newPassword,
+        password_salt,
+        password_hash,
         is_email_verified: true,
         updated_at: new Date(),
       },
-    });
+    })
 
     if (!updatedUserAccount) {
       return sendResponse(res, 400, {
-        message: "Failed to update new password",
-      });
+        message: 'Failed to update new password',
+      })
     }
 
     return sendResponse(res, 200, {
-      message: "Password updated",
-    });
+      message: 'Password updated',
+    })
   } catch (e) {
-    console.log(e.message);
+    console.log(e.message)
     return sendResponse(res, e.errorCode ? e.errorCode : 500, {
       message: e.errorCode < 500 ? e.message : 'something went wrong',
     })
   }
-};
+}
 
-export default handler;
+export default handler
