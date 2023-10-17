58261a32cbfea021240980c5d556d99dd582a03d
Harshit Vashisht <120767685+HarshitVashisht11@users.noreply.github.com>
2023-10-17 16:39:36 +0530

    Update index.js

--- open_tms_backend/open_tms_update_ticket/index.js
+++ open_tms_backend/open_tms_update_ticket/index.js
@@ -1,104 +1,105 @@
-import { shared } from '@appblocks/node-sdk'
-  const updateTicket = async (ticketId, data) => {
-  // Import Prisma models using shared
-  const Ticket = await shared.import('open_tms', 'Ticket');
-  const TicketRevision = await shared.import('open_tms', 'TicketRevision');
-  const TicketActivity = await shared.import('open_tms', 'TicketActivity');
-  const { prisma } = await shared.getShared();
+import { shared } from "@appblocks/node-sdk";
 
-  // Find the latest ticket revision for the given ticketId
-  const latestRevision = await TicketRevision.findOne({
-    where: { ticketId },
-    order: [['id', 'DESC']],
-  });
-
-  // Validate the process path to ensure the transition is valid
-  if (await validateProcessPath(latestRevision.currentStage, data.to_stage, data.assignee_id)) {
-    // Update the ticket fields
-    const ticket = await Ticket.findByPk(ticketId);
-    if (data.name) ticket.name = data.name;
-    if (data.description) ticket.description = data.description;
-
-    // Save the updated ticket
-    await ticket.save();
-
-    // Create a new ticket revision
-    const newRevision = await TicketRevision.create({
-      ticketId,
-      name: data.name || latestRevision.name,
-      description: data.description || latestRevision.description,
-    });
-
-    // Get the stage_id for the 'to_stage' from the process path
-    const stageId = await getStageIdFromProcessPath(data.to_stage);
-
-    // Create a new entry in the ticket_activity table with the correct stage_id
-    const activity = await TicketActivity.create({
-      TicketRevisionId: newRevision.id,
-      currentStage: stageId,
-    });
-
-    return { success: true, activity };
-  } else {
-    // If validation fails, throw an error
-    throw new Error('Next stage or assignee is invalid.');
-  }
-};
-
-// Get the stage_id from the process path for a given stage name
-const getStageIdFromProcessPath = async (stageName) => {
-  const ProcessPath = await shared.import('open_tms', 'ProcessPath');
-  const Stage = await shared.import('open_tms', 'Stage');
-
-  const processPath = await ProcessPath.findOne({
-    where: { from_stage: latestRevision.currentStage },
-    include: { nextStages: true },
-  });
-
-  if (!processPath) {
-    throw new Error(`Invalid current stage: ${latestRevision.currentStage}`);
-  }
-
-  const nextStageObj = processPath.nextStages.find((stage) => stage.name === stageName);
-
-  if (!nextStageObj) {
-    throw new Error(`Invalid next stage: ${stageName}`);
-  }
-
-  return nextStageObj.stage_id;
-};
-
-const handler = async (event) => {
-  const { req, res } = event;
-  const { sendResponse } = await shared.getShared();
-
-  // Health check
-  if (req.params.health === 'health') {
-    return sendResponse(res, 200, { message: 'Health check success' });
-  }
-
+const handler = async ({ req, res }) => {
   try {
-    const { ticket_id, ...updateData } = req.body;
-    const { success, activity } = await updateTicket(ticket_id, updateData);
+    const { prisma } = await shared.getShared();
 
-    if (success) {
-      return sendResponse(res, 200, { meta: { status: 200, message: 'Ticket updated successfully' }, activity });
+    // Get data from the request body
+    const {
+      ticket_id,
+      to_stage,
+      assignee_id,
+      remarks,
+      name,
+      description,
+    } = req.body;
+
+    // 1. Find the latest ticket revision
+    const latestTicketRevision = await prisma.ticket_revision.findFirst({
+      where: { ticket_id },
+      orderBy: { created_at: "desc" },
+    });
+
+    if (!latestTicketRevision) {
+      return res.status(404).json({ message: 'Ticket not found' });
     }
-  } catch (error) {
-    if (error.message === 'Next stage or assignee is invalid.') {
-      return sendResponse(res, 400, {
+
+    // 2. Find the latest ticket activity for the latest ticket revision
+    const latestTicketActivity = await prisma.ticket_activity.findFirst({
+      where: { ticket_revision_id: latestTicketRevision.id },
+      orderBy: { created_at: "desc" },
+    });
+
+    if (!latestTicketActivity) {
+      return res.status(404).json({ message: 'Ticket activity not found' });
+    }
+
+    // 3. Find the current stage from the latest ticket activity
+    const currentStage = latestTicketActivity.current_stage;
+
+    // 4. Validate the to stage and assignee with available process paths
+    const processPath = await prisma.process_path.findFirst({
+      where: { from_stage: currentStage, to_stage },
+    });
+
+    if (!processPath || processPath.process_id !== latestTicketRevision.process_id) {
+      return res.status(400).json({
         meta: { status: 400, message: 'Bad Request' },
-        errors: [{ code: 'BAD_REQUEST', message: 'Next stage or assignee is invalid.' }],
-      });
-    } else {
-      return sendResponse(res, 500, {
-        meta: { status: 500, message: 'Something went wrong.' },
-        errors: [{ code: 'INTERNAL_ERROR', message: 'Something went wrong.' }],
+        errors: [
+          { code: 'BAD_REQUEST', message: 'Next stage or assignee is invalid.' },
+        ],
       });
     }
-  }
 
-  return sendResponse(res, 200, { message: 'Successfully updated ticket' });
+    // 5. If validation passes, add a new entry to the ticket_activity table
+    const newTicketActivity = await prisma.ticket_activity.create({
+      data: {
+        created_by: req.user.id,
+        assignee_id,
+        current_stage: to_stage,
+        ticket_revision_id: latestTicketRevision.id,
+        remark: remarks,
+      },
+    });
+
+    // 6. If the ticket needs to be revised, create a new entry in the ticket_revision table
+ if (name || description) {
+  const newTicketRevision = await prisma.ticket_revision.create({
+    data: {
+      title: name || latestTicketRevision.title, // Update the title attribute
+      description: description || latestTicketRevision.description,
+      created_by: req.user.id,
+      process_id: latestTicketRevision.process_id,
+      ticket_id,
+    },
+  });
+
+  // Create a new ticket_activity entry for the revised ticket
+  await prisma.ticket_activity.create({
+    data: {
+      created_by: req.user.id,
+      current_stage: to_stage,
+      ticket_revision_id: newTicketRevision.id,
+      remark: remarks,
+    },
+  });
+}
+
+    return res.status(200).json({
+      meta: { status: 200, message: 'Ticket updated successfully' },
+      data: {
+        ticket_id,
+        ticket_revision_id: newTicketRevision?.id || latestTicketRevision.id,
+        ticket_activity_id: newTicketActivity.id,
+      },
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({
+      meta: { status: 500, message: 'Something went wrong.' },
+      errors: [{ code: 'INTERNAL_ERROR', message: 'Something went wrong.' }],
+    });
+  }
 };
 
 export default handler;
