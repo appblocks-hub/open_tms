1f54a2c245960f0e25fd118e0b54d368f2b518a0
Jishnu <jishnu.nb@neoito.com>
2023-10-19 13:59:15 +0530

    _AUTH removed envs

--- open_tms_auth/open_tms_auth_be_verify_otp/index.js
+++ open_tms_auth/open_tms_auth_be_verify_otp/index.js
@@ -1,6 +1,6 @@
-import { shared, env } from '@appblocks/node-sdk'
+import { shared, env } from "@appblocks/node-sdk";
 
-env.init()
+env.init();
 
 /**
  * @swagger
@@ -30,7 +30,7 @@ env.init()
  *         description: Ok
  */
 const handler = async (event) => {
-  const { req, res } = event
+  const { req, res } = event;
 
   const {
     sendResponse,
@@ -40,24 +40,24 @@ const handler = async (event) => {
     isEmpty,
     redis,
     generateRandomString,
-  } = await shared.getShared()
+  } = await shared.getShared();
   try {
     // health check
-    if (checkHealth(req, res)) return
+    if (checkHealth(req, res)) return;
 
-    await validateRequestMethod(req, ['POST'])
+    await validateRequestMethod(req, ["POST"]);
 
-    const requestBody = req.body
+    const requestBody = req.body;
 
     if (
       isEmpty(requestBody) ||
-      !requestBody.hasOwnProperty('email') ||
-      !requestBody.hasOwnProperty('otp') ||
+      !requestBody.hasOwnProperty("email") ||
+      !requestBody.hasOwnProperty("otp") ||
       requestBody?.otp?.length !== 6
     ) {
       return sendResponse(res, 400, {
-        message: 'Please provide a valid Email and OTP',
-      })
+        message: "Please provide a valid Email and OTP",
+      });
     }
 
     const user_account = await prisma.user_account.findFirst({
@@ -65,25 +65,25 @@ const handler = async (event) => {
         email: requestBody.email,
       },
       include: { user: true },
-    })
+    });
 
     if (!user_account) {
       return sendResponse(res, 400, {
-        message: 'Please enter a valid user id',
-      })
+        message: "Please enter a valid user id",
+      });
     }
 
-    const { user } = user_account
+    const { user } = user_account;
 
     // Retrieve the value of the key
-    if (!redis.isOpen) await redis.connect()
-    const otp = await redis.get(`${user_account.id}_otp`)
-    await redis.disconnect()
+    if (!redis.isOpen) await redis.connect();
+    const otp = await redis.get(`${user_account.id}_otp`);
+    await redis.disconnect();
 
     if (otp !== requestBody.otp) {
       return sendResponse(res, 400, {
-        message: 'Invalid OTP. Please try again or generate new otp',
-      })
+        message: "Invalid OTP. Please try again or generate new otp",
+      });
     }
 
     const updatedUser = await prisma.user_account.update({
@@ -94,32 +94,32 @@ const handler = async (event) => {
         is_email_verified: true,
         updated_at: new Date(),
       },
-    })
+    });
 
     if (updatedUser) {
-      const userAuthToken = generateRandomString(32)
+      const userAuthToken = generateRandomString(32);
       // Store the otp with an expiry stored in env.function in seconds
-      if (!redis.isOpen) await redis.connect()
+      if (!redis.isOpen) await redis.connect();
       await redis.set(userAuthToken, user_account.id, {
-        EX: Number(process.env.BB_OPEN_TMS_AUTH_OTP_EXPIRY_TIME_IN_SECONDS),
-      })
-      await redis.disconnect()
+        EX: Number(process.env.BB_OPEN_TMS_OTP_EXPIRY_TIME_IN_SECONDS),
+      });
+      await redis.disconnect();
 
       return sendResponse(res, 200, {
         data: { user_auth_token: userAuthToken },
-        message: 'OTP verified successfully',
-      })
+        message: "OTP verified successfully",
+      });
     }
 
     return sendResponse(res, 400, {
-      message: 'Invalid OTP. Please try again or generate new otp',
-    })
+      message: "Invalid OTP. Please try again or generate new otp",
+    });
   } catch (e) {
-    console.log(e.message)
+    console.log(e.message);
     return sendResponse(res, e.errorCode ? e.errorCode : 500, {
-      message: e.errorCode < 500 ? e.message : 'something went wrong',
-    })
+      message: e.errorCode < 500 ? e.message : "something went wrong",
+    });
   }
-}
+};
 
-export default handler
+export default handler;
