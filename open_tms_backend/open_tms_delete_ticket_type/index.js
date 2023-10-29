92c4b8119babbc24e0ef26dd774d9bbf9ddbdbd6
Athulya Ratheesh <athulya.r@neoito.com>
2023-10-29 17:52:32 +0530

    ise error commit

--- open_tms_backend/open_tms_delete_ticket_type/index.js
+++ open_tms_backend/open_tms_delete_ticket_type/index.js
@@ -41,51 +41,57 @@ import { shared } from '@appblocks/node-sdk'
  */
 
 const handler = async (event) => {
-  const { req, res } = event
-  const { sendResponse, isEmpty, prisma, validateRequestMethod, checkHealth } = await shared.getShared();
-  const ticket_type_id = req.body.ticket_type_id;
+  try {
+    const { req, res } = event
+    const { sendResponse, isEmpty, prisma, validateRequestMethod, checkHealth } = await shared.getShared();
+    const ticket_type_id = req.body.ticket_type_id;
 
-  if (checkHealth(req, res)) return;
+    if (checkHealth(req, res)) return;
 
-  await validateRequestMethod(req, ["POST"]);
+    await validateRequestMethod(req, ["POST"]);
 
-  if (isEmpty(ticket_type_id)) {
-    return sendResponse(res, 400, {
-      message: "Please provide ticket type id",
-    });
-  }
+    if (isEmpty(ticket_type_id)) {
+      return sendResponse(res, 400, {
+        message: "Please provide ticket type id",
+      });
+    }
 
-  const ticketType = await prisma.ticket_types.findFirst({
-    where: {
-      id: ticket_type_id,
-    },
-  });
-
-  if (!ticketType) {
-    return sendResponse(res, 400, {
-      message: "Ticket type id is invalid.",
-    });
-  }
-
-  const associatedTickets = await prisma.ticket.findMany({
-    where: {
-      ticket_type_id,
-    },
-  });
-
-  if (associatedTickets?.length > 0) {
-    return sendResponse(res, 400, {
-      message: "Cannot delete this ticket type; it is associated with tickets.",
-    });
-  } else {
-    // If there are no associated tickets, delete the ticket type
-    await prisma.ticket_types.delete({
+    const ticketType = await prisma.ticket_types.findFirst({
       where: {
         id: ticket_type_id,
       },
     });
 
-    return sendResponse(res, 200, { message: 'Successfully deleted ticket type' });
+    if (!ticketType) {
+      return sendResponse(res, 400, {
+        message: "Ticket type id is invalid.",
+      });
+    }
+
+    const associatedTickets = await prisma.ticket.findMany({
+      where: {
+        ticket_type_id,
+      },
+    });
+
+    if (associatedTickets?.length > 0) {
+      return sendResponse(res, 400, {
+        message: "Cannot delete this ticket type; it is associated with tickets.",
+      });
+    } else {
+      // If there are no associated tickets, delete the ticket type
+      await prisma.ticket_types.delete({
+        where: {
+          id: ticket_type_id,
+        },
+      });
+
+      return sendResponse(res, 200, { message: 'Successfully deleted ticket type' });
+    }
+  } catch (err) {
+    return sendResponse(res, 500, {
+      message: "Something went wrong.",
+    });
   }
 }
 
